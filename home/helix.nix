335a97b9cce84057856a20cda9a57ab768eb42f4
satoqz <satoqz@pm.me>
2022-12-07 12:39:27 +0100

    prefer deno, who uses node?

--- home/helix.nix
+++ home/helix.nix
@@ -31,11 +31,41 @@
   };
 
   # default values: https://github.com/helix-editor/helix/blob/master/languages.toml
-  programs.helix.languages = [
-    {
-      name = "nix";
-      formatter.command = "alejandra";
+  programs.helix.languages =
+    [
+      {
+        name = "nix";
+        formatter.command = "alejandra";
+        auto-format = true;
+      }
+    ]
+    ++ builtins.map ({
+      name,
+      id ? name,
+    }: {
+      inherit name;
+      roots = ["deno.json" "deno.jsonc" "tsconfig.json"];
+      config = {
+        enable = true;
+        lint = true;
+        unstable = true;
+      };
+      language-server = {
+        command = "deno";
+        args = ["lsp"];
+        language-id = id;
+      };
       auto-format = true;
-    }
-  ];
+    }) [
+      {name = "javascript";}
+      {name = "typescript";}
+      {
+        name = "jsx";
+        id = "javascriptreact";
+      }
+      {
+        name = "tsx";
+        id = "typescriptreact";
+      }
+    ];
 }
