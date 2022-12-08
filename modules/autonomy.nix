97c78d174347cc825f0018e3eae0efd9441a5d66
satoqz <satoqz@pm.me>
2022-12-08 09:16:33 +0100

    fix autonomy module

--- modules/autonomy.nix
+++ modules/autonomy.nix
@@ -1,12 +1,28 @@
-{inputs, ...}: {
+{
+  lib,
+  config,
+  inputs,
+  ...
+}: {
   system.autoUpgrade = {
     enable = true;
     flake = inputs.self.outPath;
-    dates = "Sun, 03:33:33";
+    dates = "Sun, 01:11:11";
+    flags = [
+      "--recreate-lock-file"
+      "--no-write-lock-file"
+      "-L"
+    ];
   };
 
   nix.gc = {
     automatic = true;
     dates = "Sun, 02:22:22";
+  };
+
+  virtualisation.docker.autoPrune = lib.mkIf config.virtualisation.docker.enable {
+    enable = true;
+    dates = "Sun, 03:33:33";
+    flags = ["-fa"];
   };
 }
