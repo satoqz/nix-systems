09fd5010e6987e5b963baebf6547019e9e0b5fb1
satoqz <satoqz@pm.me>
2022-12-30 14:03:40 +0100

    chore(home/starship): shorten prompt

--- home/zsh.nix
+++ home/zsh.nix
@@ -29,7 +29,7 @@
     format = "$hostname$directory$nix_shell[λ ](white)";
 
     hostname.format = "([\\(](bright-black)[host](cyan) [$hostname](white)[\\)](bright-black) )";
-    directory.format = "([\\(](bright-black)[dir](green) [$path](white)[\\)](bright-black) )";
+    directory.format = "([\\(](bright-black)[$path](white)[\\)](bright-black) )";
     nix_shell.format = "[\\(](bright-black)[\\$](blue)[\\)](bright-black) ";
 
     right_format = "$git_status$git_branch";
