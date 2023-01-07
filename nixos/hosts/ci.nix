6937e8bff13c5590adfe8e815fd0f54b651598c1
satoqz <satoqz@pm.me>
2023-01-07 21:56:23 +0100

    fix(hosts/ci): fake device path

--- nixos/hosts/ci.nix
+++ nixos/hosts/ci.nix
@@ -4,7 +4,7 @@
   ...
 }: let
   ciModule = builtins.getEnv "CI_MODULE";
-  fakeDevice = "ruby"; # does not exist
+  fakeDevice = "/ruby"; # does not exist
 in {
   imports = lib.optional (ciModule != "") self.nixosModules.${ciModule};
 
